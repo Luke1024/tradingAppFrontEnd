936791697c8964841cd2c913293cfba9e904603a
Luke1024 <chajdas.lukasz@gmail.com>
2020-03-09 20:24:04 +0100

    half of ChartGenerator written

--- src/main/java/frontend/chartDrawer/utilities/processor/utilities/test2/ChartGenerator.java
+++ src/main/java/frontend/chartDrawer/utilities/processor/utilities/test2/ChartGenerator.java
@@ -3,23 +3,26 @@ package frontend.chartDrawer.utilities.processor.utilities.test2;
 import com.vaadin.flow.component.html.Image;
 import frontend.chartDrawer.utilities.processor.utilities.test.chartParts.ChartPart;
 import frontend.chartDrawer.utilities.processor.utilities.test.chartParts.Color;
+import frontend.chartDrawer.utilities.processor.utilities.test.chartParts.Line;
 import frontend.chartDrawer.utilities.processor.utilities.test.chartParts.Rectangle;
 import frontend.client.dto.CurrencyOverviewDto;
+import frontend.client.dto.DataPointDto;
 import frontend.config.ChartConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ChartGenerator {
     @Autowired
     private ChartConfig chartConfig;
     private List<ChartPart> chartParts = new ArrayList<>();
+    private Rectangle chartBox;
 
     public Image generateChart(CurrencyOverviewDto currencyOverviewDto) {
         drawBackGround();
         drawChartBorder();
-        drawChartVerticalGrid();
+        drawChartVerticalGrid(currencyOverviewDto);
+        drawChartLine(currencyOverviewDto);
     }
 
     private void drawBackGround(){
@@ -45,8 +48,57 @@ public class ChartGenerator {
         Color borderColor = new Color(chartConfig.getBorderColorRGB());
 
         Rectangle borderRectangle = new Rectangle(borderColor, borderColor, false, x,y,width,height);
+        chartBox = borderRectangle;
         chartParts.add(borderRectangle);
     }
 
+    private void drawChartVerticalGrid(CurrencyOverviewDto currencyOverviewDto){
+        int numberOfDataPoints = currencyOverviewDto.getDataPoints().size();
 
+        int lineToLineDistanceInPixels = chartBox.getWidth()/numberOfDataPoints;
+        int minimalDistanceBetweenGridLines = chartConfig.getMinimalDistanceBetweenGridLines();
+
+        while(lineToLineDistanceInPixels >= minimalDistanceBetweenGridLines){
+            lineToLineDistanceInPixels = lineToLineDistanceInPixels/10;
+        }
+
+        int stepInPixels = chartBox.getWidth()/lineToLineDistanceInPixels;
+
+        int startingPositionX = chartBox.getX();
+        int y1 = chartBox.getY();
+        int y2 = chartBox.getY() + chartBox.getHeight();
+
+        Color lineColor = new Color(chartConfig.getGridColorRGB());
+
+        for(int i=0; i<lineToLineDistanceInPixels; i++) {
+            int x = startingPositionX + (i + 1)* stepInPixels;
+            Line line = new Line(lineColor,x,y1,x,y2);
+        }
+    }
+
+    private void drawChartLine(CurrencyOverviewDto currencyOverviewDto) {
+        int chartLineRangeInPercentage = getChartLineRangeInPercentage(chartConfig.getMaxMinHeightRangePercentage());
+
+        int topAndBottomMargin = (chartBox.getHeight() * (100-chartLineRangeInPercentage))/2;
+
+        int startingPositionX = chartBox.getX();
+        int highestPositionY1 = chartBox.getY();
+        int lowestPositionY2 = chartBox.getY() + chartBox.getHeight();
+
+        int stepInPixels = chartBox.getWidth()/currencyOverviewDto.getDataPoints().size();
+
+        double highestValuePoint = getHighestValueInDataPoints(currencyOverviewDto);
+        double lowestValuePoint = getLowestValueInDataPoints(currencyOverviewDto);
+    }
+
+    private int getChartLineRangeInPercentage(int chartRangeInPercentage){
+        if(chartRangeInPercentage > 100){
+            return 100;
+        } else return chartRangeInPercentage;
+    }
+
+    private double getHighestValueInDataPoints(CurrencyOverviewDto currencyOverviewDto){
+        List<DataPointDto> dataPoints = currencyOverviewDto.getDataPoints();
+        return dataPoints.stream().max(Comparator.comparing(DataPointDto::getValue)).map(d -> d.getValue()).orElseThrow(NoSuchElementException::new);
+    }
 }
