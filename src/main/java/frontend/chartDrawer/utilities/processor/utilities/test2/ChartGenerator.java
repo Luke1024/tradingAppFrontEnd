874307620c40d0f48a423db28358d1fa6ce89470
Luke1024 <chajdas.lukasz@gmail.com>
2020-03-14 20:16:44 +0100

    working on drawChartLine part of ChartGenerator

--- src/main/java/frontend/chartDrawer/utilities/processor/utilities/test2/ChartGenerator.java
+++ src/main/java/frontend/chartDrawer/utilities/processor/utilities/test2/ChartGenerator.java
@@ -84,11 +84,18 @@ public class ChartGenerator {
         int startingPositionX = chartBox.getX();
         int highestPositionY1 = chartBox.getY();
         int lowestPositionY2 = chartBox.getY() + chartBox.getHeight();
+        int chartHeight = chartBox.getHeight();
 
         int stepInPixels = chartBox.getWidth()/currencyOverviewDto.getDataPoints().size();
 
         double highestValuePoint = getHighestValueInDataPoints(currencyOverviewDto);
         double lowestValuePoint = getLowestValueInDataPoints(currencyOverviewDto);
+
+        List<Double> scaledValuesInDataPoints = minMaxScalling(lowestValuePoint, highestValuePoint, currencyOverviewDto);
+        List<Integer> valuesScaledToPixels = minMaxScallingToPixelValues(scaledValuesInDataPoints, chartHeight, topAndBottomMargin);
+        //List<Integer> pixelHeightValues = moveChartLine(valuesScaledToPixels,lowestPositionY2, topAndBottomMargin);
+        List<Line> chartLineLines = drawLinesBetweenPoints(valuesScaledToPixels, stepInPixels);
+
     }
 
     private int getChartLineRangeInPercentage(int chartRangeInPercentage){
@@ -100,5 +107,45 @@ public class ChartGenerator {
     private double getHighestValueInDataPoints(CurrencyOverviewDto currencyOverviewDto){
         List<DataPointDto> dataPoints = currencyOverviewDto.getDataPoints();
         return dataPoints.stream().max(Comparator.comparing(DataPointDto::getValue)).map(d -> d.getValue()).orElseThrow(NoSuchElementException::new);
+    }
+
+    private double getLowestValueInDataPoints(CurrencyOverviewDto currencyOverviewDto){
+        List<DataPointDto> dataPoints = currencyOverviewDto.getDataPoints();
+        return dataPoints.stream().min(Comparator.comparing(DataPointDto::getValue)).map(d -> d.getValue()).orElseThrow(NoSuchElementException::new);
+    }
+
+    private List<Double> minMaxScalling(double min, double max, CurrencyOverviewDto currencyOverviewDto) {
+        List<Double> scaledValues = new ArrayList<>();
+        List<DataPointDto> dataPointDtoList = currencyOverviewDto.getDataPoints();
+        for(DataPointDto dataPointDto : dataPointDtoList) {
+            double value = dataPointDto.getValue();
+            double valueScaled = (value - min)/(max - min);
+            scaledValues.add(valueScaled);
+        }
+        return scaledValues;
+    }
+
+    private List<Integer> minMaxScallingToPixelValues(List<Double> scaledValuesInDataPoints, int chartHeight, int topAndBottomMargin) {
+        int chartRangeInPixels = chartHeight - topAndBottomMargin;
+
+        List<Integer> pixelHeightValues = new ArrayList<>();
+
+        for(Double scaledValue : scaledValuesInDataPoints){
+            pixelHeightValues.add((int) (scaledValue*chartRangeInPixels));
+        }
+        return pixelHeightValues;
+    }
+
+    private List<Integer> moveChartLine(List<Integer> valuesScaledToPixels, int chartLowInPix, int margin) {
+        List<Integer> pixelHeightValues = new ArrayList<>();
+
+        for(Integer value : valuesScaledToPixels){
+            pixelHeightValues.add(value + chartLowInPix + margin);
+        }
+        return pixelHeightValues;
+    }
+
+    private List<Line> drawLinesBetweenPoints(List<Integer> valuesScaledToPixels, int stepInPixels){
+
     }
 }
