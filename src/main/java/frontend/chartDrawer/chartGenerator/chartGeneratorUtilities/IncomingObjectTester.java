d085728a58ff06ce205a2bc38c166217ef4312dc
Luke1024 <chajdas.lukasz@gmail.com>
2020-04-06 14:52:59 +0200

    Incoming Object Tester fixed

--- src/main/java/frontend/chartDrawer/chartGenerator/chartGeneratorUtilities/IncomingObjectTester.java
+++ src/main/java/frontend/chartDrawer/chartGenerator/chartGeneratorUtilities/IncomingObjectTester.java
@@ -1,9 +1,12 @@
 package frontend.chartDrawer.chartGenerator.chartGeneratorUtilities;
 
 import frontend.chartDrawer.chartGenerator.chartParts.ChartDataDto;
+import frontend.chartDrawer.chartGenerator.chartParts.ViewTimeFrame;
+import frontend.client.dto.CurrencyOverviewDto;
+import frontend.client.dto.DataPointDto;
+import frontend.config.ChartConfig;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,6 +15,57 @@ public class IncomingObjectTester {
     private boolean nullNotFound = true;
 
     public boolean isObjectsCorrect(ChartDataDto chartDataDto) {
+        boolean currencyCorrect = currencyOverviewDtoIsCorrect(chartDataDto.getCurrencyOverviewDto());
+        boolean viewCorrect = viewTimeFrameIsCorrect(chartDataDto.getViewTimeFrame());
+        boolean chartConfig = chartConfigCorrect(chartDataDto.getChartConfig());
+
+        if(currencyCorrect && viewCorrect && chartConfig) return true;
+        else return false;
+    }
+
+    private boolean currencyOverviewDtoIsCorrect(CurrencyOverviewDto currencyOverviewDto) {
+        if(currencyOverviewDto == null) return false;
+        else {
+            if(currencyOverviewDto.getCurrencyName() == null) return false;
+            if(currencyOverviewDto.getLastRetrieved() == null) return false;
+            if(currencyOverviewDto.getDataPoints() == null) return false;
+            if(dataPointsIsOk(currencyOverviewDto.getDataPoints())) return true;
+            else return false;
+        }
+    }
+
+    private boolean dataPointsIsOk(List<DataPointDto> dataPointDtoList) {
+        boolean ok = true;
+        for(DataPointDto dataPointDto : dataPointDtoList){
+            if(dataPointNotOk(dataPointDto)) ok = false;
+        }
+        return ok;
+    }
+
+    private boolean dataPointNotOk(DataPointDto dataPointDto) {
+        if(dataPointDto.getTimeStamp() == null) return true;
+        return false;
+    }
+
+
+    private boolean viewTimeFrameIsCorrect(ViewTimeFrame viewTimeFrame){
+        if(viewTimeFrame == null) return false;
+        return true;
+    }
+
+    private boolean chartConfigCorrect(ChartConfig chartConfig) {
+        if(chartConfig == null) return false;
+        else {
+            List<Field> chartConfigFields = Arrays.asList(chartConfig.getClass().getDeclaredFields());
+            for (Field field : chartConfigFields) {
+                if (field == null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    /*
         List<Field> extractedFields = Arrays.asList(chartDataDto.getClass().getDeclaredFields());
         extractAllFieldsFromObjectAndNestedObjects(extractedFields);
         return nullNotFound;
@@ -46,4 +100,5 @@ public class IncomingObjectTester {
         }
         return initialized;
     }
+    */
 }
